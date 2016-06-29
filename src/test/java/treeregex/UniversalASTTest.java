d96fc53ee6a938bab372d74ae5a53e324f344b09
Koushik Sen <ksen@starflyer.local>
2016-06-29 12:19:15 +0530

    replace tests are working

--- src/test/java/treeregex/UniversalASTTest.java
+++ src/test/java/treeregex/UniversalASTTest.java
@@ -102,4 +102,29 @@ public class UniversalASTTest {
         TreeRegexAST pattern = TreeRegexAST.parseTreeRegex("x(*b*)y", true);
         ObjectArrayList matches = pstring.matches(pattern);
     }
+
+    @Test
+    public void testReplace1() throws Exception {
+        String pstring = "982(%Hello123%)";
+        TreeRegexLib treeregex = new TreeRegexLib("(\\d+)(%([a-zA-Z_]+)(\\d+)%)", true, "$3(%$2$1%)");
+        UniversalAST mod = treeregex.replace(pstring);
+        assertEquals("123(%Hello982%)", mod + "");
+
+    }
+
+    @Test
+    public void testReplace2() throws Exception {
+        String pstring = "982(%Hello(%World%)123%)";
+        TreeRegexLib treeregex = new TreeRegexLib("(\\d+)(%([a-zA-Z_]+)@(\\d+)%)", true, "$3(%$2$1%)");
+        UniversalAST mod = treeregex.replace(pstring);
+        assertEquals("(%World%)(%Hello982%)", mod + "");
+    }
+
+    @Test
+    public void testReplace3() throws Exception {
+        String pstring = "982(%ihb(%Hello(%World(%novar%)%)(%World(%novar%)%)end%)123(%Hello(%World(%var%)%)(%World(%novar%)%)end%)end%)";
+        TreeRegexLib treeregex = new TreeRegexLib("(\\d+)(*(World)(%(var)%)*)", true, "$1$2(%Universe(%$4%)%)");
+        UniversalAST mod = treeregex.replace(pstring);
+        assertEquals("982(%ihb(%Hello(%World(%novar%)%)(%World(%novar%)%)end%)123(%Hello(%Universe(%var%)%)(%World(%novar%)%)end%)end%)", mod + "");
+    }
 }
